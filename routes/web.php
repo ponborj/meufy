dad14af3df2cdb76dfecfae9cceb1666e78a3692
Matheus Augusto <matheus.thop@gmail.com>
2025-11-12 02:47:41 -0300

    Rotas criadas

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,33 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TMDBController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
 
-Route::middleware('auth')->group(function () {
+    // ROTAS DO PERFIL DO USUÁRIO
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // ROTAS DO TMDB
+    Route::get('/tmdb/search', [TMDBController::class, 'search'])->name('tmdb.search');
+
+    // ROTAS DE FILMES
+    Route::resource('movies', MovieController::class)->names([
+        'index' => 'movies.index',
+        'create' => 'movies.create',
+        'store' => 'movies.store',
+        'edit' => 'movies.edit',
+        'update' => 'movies.update',
+        'destroy' => 'movies.destroy',
+    ]);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
